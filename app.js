5f5babaf13855c7c9e90e439bb140f1793b4a8fa
Lukas Himsel <lukas.himsel@web.de>
2016-03-24 21:35:37 +0100

    now with mapping

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 /**
  * Created by lukas on 25.02.16.
  */
+
+var keyMapping = {
+    // major
+    "C": "C",
+    "G": "G",
+    "D": "D",
+    "A": "A",
+    "E": "E",
+    "H": "B",
+    "Fis": "F#",
+    "Ges": "Gb",
+    "Des": "Db",
+    "As": "Ab",
+    "Es": "Eb",
+    "B": "Bb",
+    "F": "Fb"
+};
+var printKey = function(data){
+    var element = document.querySelector('.vex-tabdiv textarea');
+    element.value = 'tabstave tablature=false notation=true clef=treble key=' + keyMapping[data];
+    var evt = document.createEvent("HTMLEvents");
+    evt.initEvent("keyup", false, true);
+    element.dispatchEvent(evt);
+}
+
 var width = 500,
     height = 500,
     radius = Math.min(width, height) / 2;
@@ -18,8 +43,8 @@ svg.append('circle')
     .attr('fill', 'transparent')
     .attr('stroke', '#000000');
 
-var dur = ['C', 'G', 'D', 'A', 'E', 'H', 'Ges/Fis', 'Des', 'As', 'Es', 'B', 'F'];
-var moll = ['a', 'e', 'h', 'fis', 'cis', 'gis', 'es/dis', 'b', 'f', 'c', 'g', 'd'];
+var dur = ['C', 'G', 'D', 'A', 'E', 'H', 'Fis', 'Ges', 'Des', 'As', 'Es', 'B', 'F'];
+var moll = ['a', 'e', 'h', 'fis', 'cis', 'gis', 'dis', 'es', 'b', 'f', 'c', 'g', 'd'];
 
 var pie = d3.layout.pie()
     .sort(null)
@@ -37,25 +62,40 @@ var g = svg.selectAll(".arc")
     .data(pie(dur))
     .enter()
     .append("g")
-    .attr("class", "arc");
+    .attr("class", "arc")
+    .attr('transform', function(d) { return "translate(" + arc.centroid(d) + ") rotate(16.51)"; });
 
 var gMoll = svg.selectAll(".arcMoll")
     .data(pie(moll))
     .enter()
     .append("g")
-    .attr("class", "arcMoll");
+    .attr("class", "arcMoll")
+    .attr('transform', function(d) { return "translate(" + arc2.centroid(d) + ") rotate(16.51)"; });
 
 g.append("text")
-    .attr('transform', function(d) { return "translate(" + arc.centroid(d) + ") rotate(16.51)"; })
+    .attr('fill', '#FF0000')
+    .attr('text-anchor', 'middle')
+    .attr('dominant-baseline', 'central')
+    .text(function(d) { return d.data; });
+g.append("rect")
+    .attr('x', -27.5)
+    .attr('y', -10)
+    .attr('width', 55)
+    .attr('height', 20)
+    .attr('fill', 'transparent')
+    .on('click', function(d){ printKey(d.data);});
+
+
+gMoll.append("text")
     .attr('fill', '#FF0000')
     .attr('text-anchor', 'middle')
     .attr('dominant-baseline', 'central')
     .text(function(d) { return d.data; });
 
-gMoll.append("text")
-    .attr('transform', function(d) { return "translate(" + arc2.centroid(d) + ") rotate(16.51)"; })
-    .attr('fill', '#FF0000')
-    .attr('text-anchor', 'middle')
-    .attr('dominant-baseline', 'central')
-    .text(function(d) { return d.data; });
-    
+gMoll.append("rect")
+    .attr('x', -17.5)
+    .attr('y', -10)
+    .attr('width', 35)
+    .attr('height', 20)
+    .attr('fill', 'transparent')
+    .on('click', function(d){ printKey(d.data);});
